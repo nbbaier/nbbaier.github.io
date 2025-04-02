4d073f10252f1b3f26f6939374ae6c0e5213c532
Nico Baier <nico.baier@gmail.com>
2025-04-01 21:02:35 -0500

    feat: added start of presentation collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -12,11 +12,35 @@ export const collections = {
 		}),
 	}),
 	papers: defineCollection({
-		loader: file("./src/content/papers.json"),
+		loader: file("./src/content/output.json", {
+			parser: (text) => JSON.parse(text).papers,
+		}),
 		schema: z.object({
 			title: z.string(),
 			publishedIn: z.string(),
 			date: z.string().regex(/\d{4}/),
+			paperLink: z.array(
+				z.object({
+					url: z.string().url(),
+					label: z.string(),
+				}),
+			),
+			coauthors: z.array(reference("authors")).optional(),
+		}),
+	}),
+	presentations: defineCollection({
+		loader: file("./src/content/output.json", {
+			parser: (text) => JSON.parse(text).presentations,
+		}),
+		schema: z.object({
+			title: z.string(),
+			conference: z.string(),
+			location: z.string(),
+			date: z
+				.string()
+				.regex(
+					/Jan|Feb|Mar|Apr|May|June|July|Aug|Sept|Oct|Nov|Dec \d{1,2}-\d{1,2}, \d{4}/,
+				),
 			paperLink: z.array(
 				z.object({
 					url: z.string().url(),
